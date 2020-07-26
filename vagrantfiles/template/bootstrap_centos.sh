08590ffaa0e5d1a86ea3235b2deb9bdd14c4f665
Venkat Nagappan <wenkatn@gmail.com>
2020-07-26 14:36:25 +0100

    Added vagrant environment template

---
+++ vagrantfiles/template/bootstrap_centos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Enable ssh password authentication
+echo "Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+systemctl reload sshd
+
+# Set Root password
+echo "Set root password"
+echo "admin" | passwd --stdin root >/dev/null 2>&1
+
+# Set local user account
+echo "Set up local user account"
+useradd -m -s /bin/bash venkatn
+echo "admin" | passwd --stdin venkatn >/dev/null 2>&1
+echo "venkatn ALL=(ALL) NOPASWD: ALL" >> /etc/sudoers
+
+# Update bashrc file
+echo "export TERM=xterm" >> /etc/bashrc
